a8d562a11453a5c0a0a6c6c378a1204e355edaa2
Hemrajpatil5807 <hemrajpatil5807@gmail.com>
2025-10-09 17:21:23 +0530

    Setting Spring Security Username and Password

---
+++ CSMS/src/main/java/com/hp/csms/controller/ExpensesController.java
@@ -0,0 +1,12 @@
+package com.hp.csms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpensesController {
+	
+	
+
+}
---
+++ CSMS/src/main/java/com/hp/csms/controller/SitesController.java
@@ -0,0 +1,12 @@
+package com.hp.csms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sites")
+public class SitesController {
+
+	
+	
+}
